b21fb8ab7d229c1389ce9e9010b0c081d7f4731c
Pamela Zambrana <p_zf888@hotmail.com>
2023-05-16 14:40:53 -0400

    Adding some styles and the section of favourites as well.

--- dogs/main.js
+++ dogs/main.js
@@ -1,44 +1,146 @@
 console.log("hola perro");
  const URL="https://api.thedogapi.com/v1/images/search?api_key=live_rIW88oJU1Xc2tqHVHuGKrkpO74qT9sUeVbivdUOwJhO8tXsgjQuuDbXvLz7FXo4F";
- const URL_FACTS = "https://dog-api.kinduff.com/api/facts?number=5"
+ const URL_FAVOURITES="https://api.thedogapi.com/v1/favourites?api_key=live_rIW88oJU1Xc2tqHVHuGKrkpO74qT9sUeVbivdUOwJhO8tXsgjQuuDbXvLz7FXo4F";
+ const URL_FAVOURITES_DELETE=(id) => (`https://api.thedogapi.com/v1/favourites/${id}?api_key=live_rIW88oJU1Xc2tqHVHuGKrkpO74qT9sUeVbivdUOwJhO8tXsgjQuuDbXvLz7FXo4F`);
+ const URL_FACTS = "https://dogapi.dog/api/v2/facts?limit=5" 
  /* fetch(URL)
     .then(res => res.json())
     .then(data=> {
         const img = document.querySelector("img");
         img.src = data.message;
     }); */
-
+/* -----------Random images -----------*/
 async function reload(){
     await(
         fetch(URL)
             .then(res => res.json())
             .then(data=> {
-                console.log(data);
+               console.log(data);
                 const img = document.querySelector("img");
+                const favouritesButton = document.getElementById("saving-favourites-button");
+
                 img.src = data[0].url;
+                favouritesButton.onclick = () => (savingFavourites(data[0].id));
+
             })
     );
 };
+/* ---------- loading Favourites images ---------- */
+async function reloadFavourites(){
+    await(
+        fetch(URL_FAVOURITES)
+            .then(res => res.json())
+            .then(data=> {
+                console.log(data);
+                const section = document.getElementById("favourites-pics-section");
+                section.innerHTML = "";
+                const favouriteTitle = document.createElement("h2");
+                const title = document.createTextNode("My Favourite Pics...");
+                favouriteTitle.appendChild(title);
+                favouriteTitle.style = "width:100%";
+                section.appendChild(favouriteTitle);
+                data.forEach(element => {
+                    //console.log(element);
+                    const favouriteSection = document.getElementById("favourites-pics-section");
+                    const newFavourite = document.createElement("div");
+                    const img = document.createElement("img");
+                    const button = document.createElement("button");
+                    const btnText = document.createTextNode("Quitar de favoritos");
+
+                    button.appendChild(btnText);
+                    button.onclick = () => (deletingFavourites(element.id));
+                    img.src = element.image.url;
+                    img.style = "max-height: 200px; max-width: fit-content;"
+                    newFavourite.appendChild(img);
+                    newFavourite.appendChild(button);
+                    newFavourite.style= "display: flex; gap: 10px; flex-direction: column; align-items: center";
+
+                    favouriteSection.appendChild(newFavourite);
+
+                });
+            })
+            .catch(error=>{
+                const errorMessage = document.getElementById("error");
+                errorMessage.innerHTML= error;
+                //console.log(error)
+            })
+    );
+};
+/* ---------- saving fouvorites images----------- */
+async function savingFavourites(id){
+    await(
+        fetch(
+            URL_FAVOURITES, 
+            {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                image_id : id
+            })
+            }
+        )
+            .then(res => res.json())
+            .then(data=> {
+                console.log(data);
+                reloadFavourites();
+                
+            })
+            .catch(error=>{
+                const errorMessage = document.getElementById("error");
+                errorMessage.innerHTML= error;
+                //console.log(error)
+            })
+    );
+};
+/* -----------Deleting favourites---------- */
+
+async function deletingFavourites(id){
+    await(
+        fetch(
+            URL_FAVOURITES_DELETE(id),
+            {
+                method: "DELETE",
+                headers:{
+                    "Content-Type": "application/json"
+                },
+            }
+        )
+            .then(res => res.json())
+            .then(data =>{
+                console.log(data);
+                reloadFavourites();
+            }
+            )
+            .catch(error => console.log(error, console.log(URL_FAVOURITES_DELETE(id))))
+    )
+};
+/* -----------random facts---------- */
 async function generateFacts(){
     await(
         fetch(URL_FACTS)
             .then(res => res.json())
             .then( data => {
-                console.log(data);
+                //console.log(data.data[0].attributes.body);
                 const facts1 = document.getElementById("content1");
                 const facts2 = document.getElementById("content2");
                 const facts3 = document.getElementById("content3");
                 const facts4 = document.getElementById("content4");
                 const facts5= document.getElementById("content5");
 
-                facts1.innerHTML = data.facts[0];
-                facts2.innerHTML = data.facts[1];
-                facts3.innerHTML = data.facts[2];
-                facts4.innerHTML = data.facts[3];
-                facts5.innerHTML = data.facts[4];
+                facts1.innerHTML = data.data[0].attributes.body;
+                facts2.innerHTML = data.data[1].attributes.body;
+                facts3.innerHTML = data.data[2].attributes.body;
+                facts4.innerHTML = data.data[3].attributes.body;
+                facts5.innerHTML = data.data[4].attributes.body;
+            })
+            .then( error => {
+                console.log(error)
             })
     )
 }
 
 reload();
+reloadFavourites();
 generateFacts();
